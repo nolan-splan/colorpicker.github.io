8d0dc5dc72269a099f7f6abd0bbca191f8e5bb96
Nolan Splan <ntsplan@mtu.edu>
2018-04-12 21:35:08 -0500

    fixes whitespace issues for user input

--- colorTest.js
+++ colorTest.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     // ask the user if they want to use rgb or hex for generating colors
     var userInput = prompt("Welcome to the color randomizer 5000! Would you like to use RGB or hexadecimal colors? Please enter 'rgb' for RGB, or 'hex' for hexadecimal.");
-    var colorScheme = userInput.toLowerCase();
+    let colorScheme = userInput.toLowerCase();
+    colorScheme = colorScheme.trim();
 
     // if the user inputs 'rgb' into the prompt, I'm using the 'rgbGenerator()' function to generate the colors
     if (colorScheme === 'rgb' ) {
